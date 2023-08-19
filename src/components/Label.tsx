cce57c70f1348f28d4ab6f7953df491aa9f2ad7d
alternateAyush <ayushbhatt2002@gmail.com>
2023-08-19 15:36:53 +0530

    debug-19-8-02

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Label=({bgColor,title,textColor}:any)=>{
-    return <div className={`m-1 py-1 px-2 rounded-full shrink-0 ${bgColor} ${textColor} shadow`}>{title}</div>
+    return <div className={`m-1 py-1 px-2 rounded-full shrink-0 whitespace-nowrap ${bgColor} ${textColor} shadow`}>{title}</div>
 }
 
 export default Label;
